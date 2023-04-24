f91323a49549712e77aa8b32af9f27d532e484ca
ovnny2 <ovnnychannel@gmail.com>
2023-04-24 20:56:45 -0300

    criando o projeto e a classe main

---
+++ src/main/java/com/craftzn/authserver/Application.java
@@ -0,0 +1,12 @@
+package com.craftzn.authserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}

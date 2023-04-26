3421bd8fb826e8f3ca83fac86c94792c217ebc8e
ovnny2 <ovnnychannel@gmail.com>
2023-04-26 17:47:30 -0300

    configurando a conexão com o oidc provider

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # auth-server
 Dedicated to provide authentication services using Keycloak as Identification and Access Provider (IAM) using OpenId Connect and Oauth2
+
+
+- **auth-server IP** = 172.21.0.2
+
+- ![img.png](img.png)
---
+++ src/main/java/com/craftzn/authserver/config/SecurityConfig.java
@@ -0,0 +1,45 @@
+package com.craftzn.authserver.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+import org.springframework.security.core.session.SessionRegistryImpl;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
+import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
+
+@Configuration
+@EnableWebSecurity
+class SecurityConfig {
+
+    @Autowired
+    private final KeycloakLogoutHandler keycloakLogoutHandler;
+
+    SecurityConfig(KeycloakLogoutHandler keycloakLogoutHandler) {
+        this.keycloakLogoutHandler = keycloakLogoutHandler;
+    }
+
+    @Bean
+    protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
+        return new RegisterSessionAuthenticationStrategy(new SessionRegistryImpl());
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/v1/users**")
+                .hasRole("USER")
+                .anyRequest()
+                .authenticated();
+        http.oauth2Login()
+                .and()
+                .logout()
+                .addLogoutHandler(keycloakLogoutHandler)
+                .logoutSuccessUrl("/v1/logout");
+        http.oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
+        return http.build();
+    }
+}

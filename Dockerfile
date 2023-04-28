da28c5715b464c1981f758e7891e22207db9ef91
ovnny2 <ovnnychannel@gmail.com>
2023-04-28 18:42:24 -0300

    refatorando parâmetros de build das imagens docker e adicionando JwtDecoder às configs de segurança

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,6 @@ WORKDIR /
 ARG JAR_FILE=/build/libs/*.jar
 
 COPY ${JAR_FILE} app.jar
-ENTRYPOINT ["java", "-jar", "/app.jar", "-b", "0.0.0.0", "--debug"]
+ENTRYPOINT ["java","-jar","/app.jar","--debug"]
 
 EXPOSE 8081
--- src/main/java/com/craftzn/authserver/config/SecurityConfig.java
+++ src/main/java/com/craftzn/authserver/config/SecurityConfig.java
@@ -7,6 +7,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.core.session.SessionRegistryImpl;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtDecoders;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
 import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
@@ -25,6 +27,11 @@ class SecurityConfig {
     @Bean
     protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
         return new RegisterSessionAuthenticationStrategy(new SessionRegistryImpl());
+    }
+
+    @Bean
+    public JwtDecoder jwtDecoder() {
+        return JwtDecoders.fromIssuerLocation("http://keycloak-sso:8080/realms/craftzn");
     }
 
     @Bean

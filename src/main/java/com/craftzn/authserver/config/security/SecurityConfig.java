7fb062485d996d5481e2bb01c6391889f95dbf84
ovnny2 <ovnnychannel@gmail.com>
2023-08-07 23:46:07 -0300

    resolvendo o problema com Dependency Injection e Inversao de Controle do resource server

--- src/main/java/com/craftzn/authserver/config/security/SecurityConfig.java
+++ src/main/java/com/craftzn/authserver/config/security/SecurityConfig.java
@@ -1,26 +1,36 @@
 package com.craftzn.authserver.config.security;
 
+import com.craftzn.authserver.config.keycloak.ResourceServer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtDecoders;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 class SecurityConfig {
 
+    ResourceServer resourceServer;
+
+    @Autowired
+    public SecurityConfig(ResourceServer resourceServer) {
+        this.resourceServer = resourceServer;
+    }
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
 
-        http.authorizeRequests()
-                .antMatchers("/v1/notifications**")
-                .hasRole("USER")
-                .anyRequest()
-                .authenticated();
-
         return http.build();
+    }
+
+    @Bean
+    public JwtDecoder jwtDecoder() {
+        return JwtDecoders.fromIssuerLocation(resourceServer.getJwt().getIssuerUri());
     }
 }
